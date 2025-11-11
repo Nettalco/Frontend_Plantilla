78e06cac2a1f55d810b3b577db17ba4253780bd7
Diego Denilson Sullcaray Ramos <205557909+DSR124124@users.noreply.github.com>
2025-11-11 12:46:14 -0500

    Enhance Angular configuration: add file replacements for environment settings in angular.json and improve layout resolver to handle errors gracefully when fetching menu items.

---
+++ src/environment/global.production.ts
@@ -0,0 +1,20 @@
+export const sistema: number = 1;
+
+export var version: string = `Versión 1.0.0`;
+export var titulo: string = `Cotizaciones`;
+export var tituloVersion: string = `v(1.0.0)`;
+
+export var urlAuth: string = 'https://nocb.nettalco.com.pe/';
+
+export var url: string = 'https://nocb.nettalco.com.pe/CotizacionesWS/';
+
+export var urlAcces: string = 'https://nocb.nettalco.com.pe/PlantillaWSPrueba/';
+
+export var urlZeus: string = 'https://nocb.nettalco.com.pe/RepoCotiWS/';
+
+export var anio: number = 2025;
+
+export var baseUrlReportes = 'https://nocb.nettalco.com.pe/RepoCotiWS/';
+
+export var tiempoInactividadAlarma: number = 8 * (60 * 60 * 1000); //X (unidades horas)
+
